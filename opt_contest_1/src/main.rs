b7773c60e601fb76730b9502dca841ae4b359962
Radu Stefan MIHALACHE <rmihalache@bitdefender.com>
2023-11-02 17:35:11 +0200

    write_all fn

--- opt_contest_1/src/main.rs
+++ opt_contest_1/src/main.rs
@@ -54,9 +54,9 @@ fn rotate(input_path: &str, output_path: &str, rot_by: u8) {
             break;
         }
 
-        rotate_in_place(&mut buf_mem, rot_by);
+        rotate_in_place(&mut buf_mem[0..n-1], rot_by);
 
-        match output.write(&buf_mem) {
+        match output.write_all(&buf_mem[0..n-1]) {
             Ok(_) => (),
             Err(err) => println!("Error writing to output file {err}")
         }
